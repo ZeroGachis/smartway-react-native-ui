2bb866e9c10cd7bb2f0b6cc718083dbdcebaa46b
ducdetronquito <guillaume.paulet@giome.fr>
2023-02-10 10:24:53 +0100

    🐛 Fix breaking change in sample app

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,17 +1,16 @@
 import * as React from 'react';
 import { StyleSheet, View } from 'react-native';
-import { Provider as PaperProvider } from 'react-native-paper';
-import { Button, Logo, theme } from 'smartway-react-native-ui';
+import { Button, Logo, ThemeProvider } from 'smartway-react-native-ui';
 
 export default function App() {
 
   return (
-    <PaperProvider theme={theme}>
+    <ThemeProvider>
       <View style={styles.container}>
         <Button icon="camera" mode="contained">Example</Button>
         <Logo size='small' />
       </View>
-    </PaperProvider>
+    </ThemeProvider>
   );
 }
 
