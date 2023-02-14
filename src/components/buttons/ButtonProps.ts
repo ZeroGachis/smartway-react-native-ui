152024fe12fd9c8ec803cb19e12458407f582e6d
Audrey Brouard <a.brouard@smartway.ai>
2023-02-14 10:12:16 +0100

    ✨ Button - Add filled variant

---
+++ src/components/buttons/ButtonProps.ts
@@ -0,0 +1,9 @@
+import type { ReactNode } from "react";
+import type { TextStyle, ViewStyle } from "react-native";
+
+
+export interface ButtonProps {
+    children?: ReactNode,
+    style?: ViewStyle,
+    labelStyle?: TextStyle,
+}
