0555cbbb360ae5ac725a048914d7d7c7e9fa08b5
Audrey Brouard <a.brouard@smartway.ai>
2023-02-17 09:59:18 +0100

    ♻️ Button - Use BaseButtonProps in Button

--- src/components/buttons/Button.tsx
+++ src/components/buttons/Button.tsx
@@ -1,18 +1,14 @@
-import React, { ReactNode } from "react";
-import type { TextStyle, ViewStyle } from "react-native";
+import React from "react";
 import { TextButton } from "./TextButton";
 import { Button as BaseButton } from 'react-native-paper';
 import { FilledButton } from "./FilledButton";
+import type { BaseButtonProps } from "./BaseButtonProps";
 
-interface ButtonProps {
+interface ButtonProps extends BaseButtonProps {
     mode?: 'text' | 'filled';
-    children?: ReactNode,
-    style?: ViewStyle,
-    labelStyle?: TextStyle,
-    onClick?: () => void,
 }
 
-export const Button = ({ mode = 'text', children, style, labelStyle, onClick}: ButtonProps) => {
+export const Button = ({ mode = 'text', children, style, labelStyle, onClick }: ButtonProps) => {
     if (mode === 'text') {
         return <TextButton style={style} labelStyle={labelStyle} onClick={onClick}>{children}</TextButton>;
     }
