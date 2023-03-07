ab2792de4a05d764b44ddf8a48d4e640095f6816
Vadim Saranov <jolantacizyte@MBP-Jolanta.lan>
2023-03-07 14:30:01 +0200

    added icon set

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,3 @@
 import { Body, Button, Card, Dialog, Headline, Icon, Logo, Screen } from './components';
 import { ThemeProvider, useTheme } from './styles/themes';
-
 export { Body, Button, Card, Dialog, Headline, Icon, Logo, Screen, ThemeProvider, useTheme };
