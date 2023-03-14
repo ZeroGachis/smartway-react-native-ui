31e4be132e65f4c47a45fcb3fe222ffbf7a10074
Vadim Saranov <jolantacizyte@MBP-Jolanta.lan>
2023-03-14 10:14:50 +0200

    :sparkles: Add new component keyboard

--- example/src/Keyboard/KeyboardPage.tsx
+++ example/src/Keyboard/KeyboardPage.tsx
@@ -1,109 +1,116 @@
-import React from 'react';
-import { Text, View } from 'react-native';
-import { Icon } from 'smartway-react-native-ui';
-
+import React, { useRef, useState } from 'react';
+import {
+    Alert,
+    Dimensions,
+    Keyboard,
+    Pressable,
+    StyleSheet,
+    Text,
+    TextInput,
+    View,
+} from 'react-native';
+const { height: SCREEN_HEIGHT } = Dimensions.get('screen');
 export const KeyboardPage = () => {
-    interface DropDownOptions<T> {
-        nameToDisplayed: string;
-        T: T;
-    }
+    const [value, setValue] = useState('');
+
+    const textInputRef = useRef<TextInput>(null);
+    const styles = StyleSheet.create({
+        button: {
+            height: SCREEN_HEIGHT * 0.0671,
+            width: '30%',
+            margin: 5,
+        },
+    });
+
+    const handlePress = (input: string) => {
+        setValue(value + input);
+        textInputRef.current?.focus();
+    };
+    const handleDelete = () => {
+        setValue(value.slice(0, -1));
+    };
     return (
-        <View style={{ padding: 16 }}>
-            <View style={{ flexDirection: 'row' }}>
-                <View style={{ height: 40, width: 40, backgroundColor: 'green', margin: 4 }}>
-                    <Text style={{ fontSize: 24, textAlign: 'center', justifyContent: 'center' }}>
+        <View style={{ marginHorizontal: 5, marginTop: 50, backgroundColor: 'white' }}>
+            <TextInput showSoftInputOnFocus={false} ref={textInputRef} value={value}></TextInput>
+            <View
+                style={{
+                    flexDirection: 'row',
+                    justifyContent: 'center',
+                    overflow: 'hidden',
+                }}
+            >
+                <Pressable style={[styles.button]} onPress={() => handlePress('1')}>
+                    <Text
+                        style={{
+                            fontSize: 24,
+                            textAlign: 'center',
+                            justifyContent: 'center',
+                        }}
+                    >
                         1
                     </Text>
-                </View>
-                <View style={{ height: 40, width: 40, backgroundColor: 'green', margin: 4 }}>
+                </Pressable>
+                <Pressable style={styles.button} onPress={() => handlePress('2')}>
                     <Text style={{ fontSize: 24, textAlign: 'center', justifyContent: 'center' }}>
                         2
                     </Text>
-                </View>
-                <View style={{ height: 40, width: 40, backgroundColor: 'green', margin: 4 }}>
+                </Pressable>
+                <Pressable style={styles.button} onPress={() => handlePress('3')}>
                     <Text style={{ fontSize: 24, textAlign: 'center', justifyContent: 'center' }}>
                         3
                     </Text>
-                </View>
-                <View style={{ height: 40, width: 40, backgroundColor: 'green', margin: 4 }}>
-                    <Text style={{ fontSize: 24, textAlign: 'center', justifyContent: 'center' }}>
-                        -
-                    </Text>
-                </View>
+                </Pressable>
             </View>
-            <View style={{ flexDirection: 'row' }}>
-                <View style={{ height: 40, width: 40, backgroundColor: 'green', margin: 4 }}>
+            <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
+                <Pressable style={styles.button} onPress={() => handlePress('4')}>
                     <Text style={{ fontSize: 24, textAlign: 'center', justifyContent: 'center' }}>
                         4
                     </Text>
-                </View>
-                <View style={{ height: 40, width: 40, backgroundColor: 'green', margin: 4 }}>
+                </Pressable>
+                <Pressable style={styles.button} onPress={() => handlePress('5')}>
                     <Text style={{ fontSize: 24, textAlign: 'center', justifyContent: 'center' }}>
                         5
                     </Text>
-                </View>
-                <View style={{ height: 40, width: 40, backgroundColor: 'green', margin: 4 }}>
+                </Pressable>
+                <Pressable style={styles.button} onPress={() => handlePress('6')}>
                     <Text style={{ fontSize: 24, textAlign: 'center', justifyContent: 'center' }}>
                         6
                     </Text>
-                </View>
-                <View style={{ height: 40, width: 40, backgroundColor: 'green', margin: 4 }}>
-                    <Text style={{ fontSize: 12, textAlign: 'center', justifyContent: 'center' }}>
-                        space
-                    </Text>
-                </View>
+                </Pressable>
             </View>
-            <View style={{ flexDirection: 'row' }}>
-                <View style={{ height: 40, width: 40, backgroundColor: 'green', margin: 4 }}>
+            <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
+                <Pressable style={styles.button} onPress={() => handlePress('7')}>
                     <Text style={{ fontSize: 24, textAlign: 'center', justifyContent: 'center' }}>
                         7
                     </Text>
-                </View>
-                <View style={{ height: 40, width: 40, backgroundColor: 'green', margin: 4 }}>
+                </Pressable>
+                <Pressable style={styles.button} onPress={() => handlePress('8')}>
                     <Text style={{ fontSize: 24, textAlign: 'center', justifyContent: 'center' }}>
                         8
                     </Text>
-                </View>
-                <View style={{ height: 40, width: 40, backgroundColor: 'green', margin: 4 }}>
+                </Pressable>
+                <Pressable style={styles.button} onPress={() => handlePress('9')}>
                     <Text style={{ fontSize: 24, textAlign: 'center', justifyContent: 'center' }}>
                         9
                     </Text>
-                </View>
-                <View style={{ height: 40, width: 40, backgroundColor: 'green', margin: 4 }}>
+                </Pressable>
+            </View>
+            <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
+                <Pressable style={styles.button} onPress={handleDelete}>
                     <Text style={{ fontSize: 24, textAlign: 'center', justifyContent: 'center' }}>
                         del
                     </Text>
-                </View>
-            </View>
-            <View style={{ flexDirection: 'row' }}>
-                <View style={{ height: 40, width: 40, backgroundColor: 'green', margin: 4 }}>
-                    <Text style={{ fontSize: 24, textAlign: 'center', justifyContent: 'center' }}>
-                        *#
-                    </Text>
-                </View>
-                <View style={{ height: 40, width: 40, backgroundColor: 'green', margin: 4 }}>
+                </Pressable>
+                <Pressable style={styles.button} onPress={() => handlePress('0')}>
                     <Text style={{ fontSize: 24, textAlign: 'center', justifyContent: 'center' }}>
                         0
                     </Text>
-                </View>
-                <View style={{ height: 40, width: 40, backgroundColor: 'green', margin: 4 }}>
+                </Pressable>
+                <Pressable style={[styles.button]} onPress={() => Alert.alert("Let's go")}>
                     <Text style={{ fontSize: 24, textAlign: 'center', justifyContent: 'center' }}>
-                        .
+                        OK
                     </Text>
-                </View>
-                <View
-                    style={{
-                        height: 40,
-                        width: 40,
-                        backgroundColor: 'green',
-                        margin: 4,
-                        justifyContent: 'center',
-                    }}
-                >
-                    <Text style={{ fontSize: 24, textAlign: 'center', justifyContent: 'center' }}>
-                        <Icon name="arrow-right" />
-                    </Text>
-                </View>
+                </Pressable>
             </View>
         </View>
     );
