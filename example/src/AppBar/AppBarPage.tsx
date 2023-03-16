865d6244cd1b03dc937435fc581797a5ae7ad163
Vadim Saranov <jolantacizyte@MBP-Jolanta.lan>
2023-03-16 20:10:42 +0200

    :adhesive_bandage: Remove unnecessary key from interface dropdownoption

--- example/src/AppBar/AppBarPage.tsx
+++ example/src/AppBar/AppBarPage.tsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { View } from 'react-native';
 import { AppBar, Menu, useTheme } from 'smartway-react-native-ui';
+import type { DropDownOption } from 'src/components/dropDown/DropDown';
 
 export const AppBarPage = () => {
     const theme = useTheme();
@@ -13,14 +14,36 @@ export const AppBarPage = () => {
         { title: 'option 4', onPress: () => console.log('option') },
     ];
 
+    const options: DropDownOption[] = [
+        { value: 'Revalorisation auto' },
+        { value: 'Option 2' },
+        { value: 'Option 3' },
+        { value: 'Option 4' },
+    ];
+    const [selected, setSelected] = useState<DropDownOption>();
+
+    useEffect(() => {
+        setSelected(options[0]);
+    }, []);
+
     return (
-        <View>
-            <AppBar onPress={() => {}} text={'Title'}>
+        <View style={{ backgroundColor: 'white', flex: 1 }}>
+            <AppBar onPress={() => {}} label={'Title'}>
                 <Menu
                     icon="more"
                     onDismiss={() => setMenuVisible(false)}
                     visible={setMenu}
                     style={{ paddingRight: theme.sw.spacing.m }}
+                    onPress={() => setMenuVisible(true)}
+                    options={menuOptions}
+                />
+            </AppBar>
+            <AppBar onPress={() => console.log('f')} label={selected?.value} type="accordion">
+                <Menu
+                    icon="settings"
+                    onDismiss={() => setMenuVisible(false)}
+                    visible={setMenu}
+                    style={{ position: 'absolute', right: 16, top: 12 }}
                     onPress={() => setMenuVisible(true)}
                     options={menuOptions}
                 />
--- src/components/appBar/AppBar.tsx
+++ src/components/appBar/AppBar.tsx
@@ -1,17 +1,28 @@
 import React from 'react';
-import { StyleSheet, View, ViewStyle } from 'react-native';
+import { StyleSheet, TouchableOpacity, View, ViewStyle } from 'react-native';
 import { Appbar as BaseAppBar } from 'react-native-paper';
 import { useTheme } from '../../styles/themes';
+import { Icon } from '../icons/Icon';
+import { Body } from '../typography/Body';
 import { Headline } from '../typography/Headline';
 
+type AppBarType = 'default' | 'accordion';
 interface Props {
-    text: string;
+    label?: string;
     onPress: () => void;
     iconName?: string;
     style?: ViewStyle;
     children?: JSX.Element;
+    type?: AppBarType;
 }
-export const AppBar = ({ text, onPress, iconName = 'arrow-left', style, children }: Props) => {
+export const AppBar = ({
+    label,
+    onPress,
+    iconName = 'arrow-left',
+    style,
+    children,
+    type = 'default',
+}: Props) => {
     const theme = useTheme();
 
     const styles = StyleSheet.create({
@@ -26,20 +37,51 @@ export const AppBar = ({ text, onPress, iconName = 'arrow-left', style, children
         appBar: {
             backgroundColor: theme.sw.colors.neutral[50],
         },
+        container: {
+            flexDirection: 'row',
+            justifyContent: 'center',
+        },
+        touchableOpacity: {
+            paddingHorizontal: theme.sw.spacing.l,
+            paddingVertical: theme.sw.spacing.m,
+            borderRadius: 8,
+            flexDirection: 'row',
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: '#ECF0F4',
+            ...style,
+        },
+        body: {
+            fontWeight: '700',
+            paddingRight: theme.sw.spacing.m,
+        },
     });
-    return (
-        <View style={styles.header}>
-            <BaseAppBar style={styles.appBar}>
-                <BaseAppBar.Action
-                    style={styles.appBarStyle}
-                    color={theme.sw.colors.neutral[800]}
-                    size={theme.sw.spacing.l}
-                    icon={iconName}
-                    onPress={onPress}
-                />
-                <Headline size="h3">{text}</Headline>
-            </BaseAppBar>
-            {children}
-        </View>
-    );
+
+    if (type === 'default') {
+        return (
+            <View style={styles.header}>
+                <BaseAppBar style={styles.appBar}>
+                    <BaseAppBar.Action
+                        style={styles.appBarStyle}
+                        color={theme.sw.colors.neutral[800]}
+                        size={theme.sw.spacing.l}
+                        icon={iconName}
+                        onPress={onPress}
+                    />
+                    <Headline size="h3">{label}</Headline>
+                </BaseAppBar>
+                {children}
+            </View>
+        );
+    } else {
+        return (
+            <View style={styles.container}>
+                <TouchableOpacity onPress={onPress} style={styles.touchableOpacity}>
+                    <Body style={styles.body}>{label}</Body>
+                    <Icon name="arrow-down" />
+                </TouchableOpacity>
+                {children}
+            </View>
+        );
+    }
 };
