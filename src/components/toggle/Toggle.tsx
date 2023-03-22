5a71442b63a4c8006337409ff5218f03bbafc786
Vadim Saranov <jolantacizyte@MacBook-Pro-Jolanta.local>
2023-03-22 16:28:55 +0200

    :bug: Make toggle component in another way to avoid color change on every re-render

--- src/components/toggle/Toggle.tsx
+++ src/components/toggle/Toggle.tsx
@@ -1,8 +1,14 @@
-import React, { useState } from 'react';
-import { Pressable, StyleSheet, TextStyle, ViewStyle } from 'react-native';
-import { Animated, Easing, View } from 'react-native';
-import { useTheme } from '../../styles/themes';
-import { Body } from '../typography/Body';
+import { StyleSheet, TextStyle, TouchableWithoutFeedback, ViewStyle } from 'react-native';
+import React, { useEffect } from 'react';
+import Animated, {
+    interpolateColor,
+    useSharedValue,
+    useAnimatedStyle,
+    withSpring,
+    withTiming,
+    useDerivedValue,
+} from 'react-native-reanimated';
+import { useTheme } from '../..//styles/themes';
 
 interface Props {
     isToggled: boolean;
@@ -15,107 +21,87 @@ interface Props {
 
 export const Toggle = ({ isToggled, onValueChange, style, text, textStyle }: Props) => {
     const theme = useTheme();
+    const knobTranslate = useSharedValue(0);
+    const textTranslate = useSharedValue(0);
 
-    const [fadeAnim] = useState(new Animated.Value(0));
+    const progress = useDerivedValue(() => {
+        return withTiming(isToggled ? 0 : 1);
+    });
 
-    React.useEffect(() => {
-        Animated.timing(fadeAnim, { toValue: 1, useNativeDriver: true });
-    }, []);
-
-    const getColor = () => {
+    useEffect(() => {
         if (isToggled) {
-            return {
-                backgroundColor: theme.sw.colors.success[500],
-                color: theme.sw.colors.neutral[50],
-            };
+            knobTranslate.value = 40;
+            textTranslate.value = 10;
+        } else {
+            knobTranslate.value = 6;
+            textTranslate.value = 50;
         }
+    }, [isToggled, knobTranslate, textTranslate]);
+
+    const moveKnob = useAnimatedStyle(() => {
         return {
-            backgroundColor: theme.sw.colors.neutral[400],
-            color: theme.sw.colors.neutral[600],
+            transform: [
+                {
+                    translateX: withSpring(knobTranslate.value, {
+                        mass: 1,
+                        damping: 15,
+                        stiffness: 120,
+                    }),
+                },
+            ],
         };
-    };
-
-    const { color } = getColor();
-
-    const animatedValue = new Animated.Value(0);
-    const moveToggle = animatedValue.interpolate({
-        inputRange: [0, 1],
-        outputRange: [6, 40],
-    });
-    const moveText = animatedValue.interpolate({
-        inputRange: [0, 1],
-        outputRange: [50, 10],
-    });
-    const animateColors = animatedValue.interpolate({
-        inputRange: [0, 1],
-        outputRange: [theme.sw.colors.neutral[400], theme.sw.colors.success[500]],
     });
 
-    animatedValue.setValue(isToggled ? 0 : 1);
+    const moveText = useAnimatedStyle(() => {
+        return {
+            left: withSpring(textTranslate.value, {
+                mass: 1,
+                damping: 15,
+                stiffness: 120,
+            }),
+        };
+    });
 
-    Animated.timing(animatedValue, {
-        toValue: isToggled ? 1 : 0,
-        duration: 300,
-        easing: Easing.linear,
-        useNativeDriver: false,
-    }).start();
+    const backgroundColorStyle = useAnimatedStyle(() => {
+        const backgroundColor = interpolateColor(
+            progress.value,
+            [0, 1],
+            [theme.sw.colors.success[500], theme.sw.colors.neutral[400]],
+        );
+        return {
+            backgroundColor,
+        };
+    });
 
     const styles = StyleSheet.create({
         container: {
-            flexDirection: 'row',
-            alignItems: 'center',
-            marginHorizontal: theme.sw.spacing.m,
-        },
-        toggleContainer: {
             width: 70,
-            paddingVertical: theme.sw.spacing.xs,
+            height: 32,
             borderRadius: 50,
             justifyContent: 'center',
+            ...style,
         },
-        toggleWheelStyle: {
+        knob: {
             width: 22.5,
             height: 22.5,
-            backgroundColor: 'white',
-            borderRadius: 12.5,
+            borderRadius: 50,
+            backgroundColor: theme.sw.colors.neutral[50],
             zIndex: 1,
         },
-        percentContainer: {
-            color: theme.sw.colors.neutral[600],
-            position: 'absolute',
-            right: 0,
-        },
         text: {
-            color,
+            position: 'absolute',
+            color: isToggled ? theme.sw.colors.neutral[50] : theme.sw.colors.neutral[600],
+            left: 50,
             ...textStyle,
         },
     });
 
     return (
-        <View style={styles.container}>
-            <Pressable onPress={onValueChange}>
-                <Animated.View
-                    style={[styles.toggleContainer, style, { backgroundColor: animateColors }]}
-                >
-                    <Animated.View
-                        style={[
-                            styles.toggleWheelStyle,
-                            {
-                                marginLeft: moveToggle,
-                            },
-                        ]}
-                    />
-                    <Animated.View
-                        style={[
-                            styles.percentContainer,
-                            {
-                                left: moveText,
-                            },
-                        ]}
-                    >
-                        {text && <Body style={styles.text}>{text}</Body>}
-                    </Animated.View>
-                </Animated.View>
-            </Pressable>
-        </View>
+        <TouchableWithoutFeedback onPress={onValueChange}>
+            <Animated.View style={[styles.container, backgroundColorStyle]}>
+                <Animated.View style={[styles.knob, moveKnob]} />
+                <Animated.Text style={[styles.text, moveText]}>{text}</Animated.Text>
+            </Animated.View>
+        </TouchableWithoutFeedback>
     );
 };
