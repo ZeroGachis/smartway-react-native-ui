576ce6db951835142c83546ca33191b2d73d5ff8
Vadim Saranov <jolantacizyte@MacBook-Pro-Jolanta.local>
2023-03-29 16:23:30 +0300

    :sparkles: Add all label cases as a property

--- example/src/Label/LabelPage.tsx
+++ example/src/Label/LabelPage.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { Label, Screen, useTheme } from 'smartway-react-native-ui';
 
 export const LabelPage = () => {
@@ -12,23 +12,46 @@ export const LabelPage = () => {
         label: {
             margin: theme.sw.spacing.xs,
         },
+        row: { flexDirection: 'row' },
     });
 
     return (
         <Screen style={styles.container}>
-            <Label
-                style={styles.label}
-                bgColor={theme.sw.colors.secondary[200]}
-                textColor={theme.sw.colors.secondary[600]}
-                text="Label"
-            />
-            <Label
-                style={styles.label}
-                text="Label"
-                bgColor={theme.sw.colors.primary[400]}
-                textColor={theme.sw.colors.neutral[50]}
-            />
-            <Label style={styles.label} text="Label" />
+            <View style={styles.row}>
+                <Label style={styles.label} type="filled" text="Label" />
+                <Label style={styles.label} type="soft" text="Label" />
+                <Label style={styles.label} type="outlined" text="Label" />
+            </View>
+            <View style={styles.row}>
+                <Label style={styles.label} type="filled" labelColor="primary" text="Label" />
+                <Label style={styles.label} type="soft" labelColor="primary" text="Label" />
+                <Label style={styles.label} type="outlined" labelColor="primary" text="Label" />
+            </View>
+            <View style={styles.row}>
+                <Label style={styles.label} type="filled" labelColor="secondary" text="Label" />
+                <Label style={styles.label} type="soft" labelColor="secondary" text="Label" />
+                <Label style={styles.label} type="outlined" labelColor="secondary" text="Label" />
+            </View>
+            <View style={styles.row}>
+                <Label style={styles.label} type="filled" labelColor="information" text="Label" />
+                <Label style={styles.label} type="soft" labelColor="information" text="Label" />
+                <Label style={styles.label} type="outlined" labelColor="information" text="Label" />
+            </View>
+            <View style={styles.row}>
+                <Label style={styles.label} type="filled" labelColor="success" text="Label" />
+                <Label style={styles.label} type="soft" labelColor="success" text="Label" />
+                <Label style={styles.label} type="outlined" labelColor="success" text="Label" />
+            </View>
+            <View style={styles.row}>
+                <Label style={styles.label} type="filled" labelColor="warning" text="Label" />
+                <Label style={styles.label} type="soft" labelColor="warning" text="Label" />
+                <Label style={styles.label} type="outlined" labelColor="warning" text="Label" />
+            </View>
+            <View style={styles.row}>
+                <Label style={styles.label} type="filled" labelColor="error" text="Label" />
+                <Label style={styles.label} type="soft" labelColor="error" text="Label" />
+                <Label style={styles.label} type="outlined" labelColor="error" text="Label" />
+            </View>
         </Screen>
     );
 };
--- src/components/label/Label.tsx
+++ src/components/label/Label.tsx
@@ -4,30 +4,85 @@ import { useTheme } from '../../styles/themes';
 
 import { Body } from '../typography/Body';
 
+type LabelType = 'outlined' | 'filled' | 'soft';
+type LabelColor =
+    | 'error'
+    | 'warning'
+    | 'success'
+    | 'information'
+    | 'secondary'
+    | 'primary'
+    | 'neutral';
+
 interface Props {
     style?: ViewStyle;
     text: string;
-    borderColor?: string;
-    textColor?: string;
-    bgColor?: string;
+    type: LabelType;
+    labelColor?: LabelColor;
 }
 
-export const Label = ({ text, borderColor, textColor, bgColor, style }: Props) => {
+export const Label = ({ style, text, type, labelColor = 'neutral' }: Props) => {
     const theme = useTheme();
+
+    const transparencyValue = '29';
+
+    const getColors = () => {
+        if (labelColor === 'neutral') {
+            switch (type) {
+                case 'filled':
+                    return {
+                        backgroundColor: theme.sw.colors[labelColor][400],
+                        color: theme.sw.colors.neutral[800],
+                    };
+                case 'soft':
+                    return {
+                        backgroundColor: theme.sw.colors[labelColor][500] + transparencyValue,
+                        color: theme.sw.colors.neutral[800],
+                    };
+                case 'outlined':
+                    return {
+                        borderColor: theme.sw.colors[labelColor][400],
+                        color: theme.sw.colors.neutral[800],
+                    };
+            }
+        }
+        switch (type) {
+            case 'filled':
+                return {
+                    backgroundColor: theme.sw.colors[labelColor][400],
+                    color:
+                        labelColor === 'warning'
+                            ? theme.sw.colors.neutral[800]
+                            : theme.sw.colors.neutral[50],
+                };
+            case 'soft':
+                return {
+                    backgroundColor: theme.sw.colors[labelColor][400] + transparencyValue,
+                    color: theme.sw.colors[labelColor][600],
+                };
+            case 'outlined':
+                return {
+                    borderColor: theme.sw.colors[labelColor][400],
+                    color: theme.sw.colors[labelColor][400],
+                };
+        }
+    };
+    const { backgroundColor, color, borderColor } = getColors();
+
     const styles = StyleSheet.create({
         container: {
+            borderWidth: type === 'outlined' ? 1 : 0,
+            backgroundColor,
             paddingHorizontal: theme.sw.spacing.xs,
             paddingVertical: 2,
-            backgroundColor: bgColor ? bgColor : theme.sw.colors.neutral[400],
-            borderWidth: borderColor ? 1 : 0,
             borderRadius: 6,
             borderColor,
             ...style,
         },
         text: {
-            color: textColor ? textColor : theme.sw.colors.neutral[800],
             lineHeight: 20,
             fontWeight: '700',
+            color,
         },
     });
     return (
