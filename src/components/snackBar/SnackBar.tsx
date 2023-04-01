5899e23b41022063266b248789671f69a5b253ec
Vadim Saranov <jolantacizyte@MBP-Jolanta.lan>
2023-04-01 23:08:08 +0300

    :bug: Add onclose property to snackbar to be able to close it without action

--- src/components/snackBar/SnackBar.tsx
+++ src/components/snackBar/SnackBar.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, StyleSheet, Text, Pressable, ViewStyle } from 'react-native';
 import { Snackbar } from 'react-native-paper';
-import { useTheme } from '../../styles/themes';
+import { useTheme } from 'src/styles/themes';
 import { Icon } from '../icons/Icon';
 import type { IconName } from '../icons/IconProps';
 
@@ -10,8 +10,9 @@ interface Props {
     message: string;
     actionLabel?: string;
     iconName?: IconName;
-    onDismiss: () => void;
+    onDismiss?: () => void;
     duration?: number;
+    onClose: () => void;
 }
 
 export const SnackBar = ({
@@ -21,19 +22,20 @@ export const SnackBar = ({
     iconName,
     duration = 4000,
     visible,
+    onClose,
 }: Props) => {
     const theme = useTheme();
 
     const getStyles = () => {
-        if (actionLabel && actionLabel.length < 10) {
+        if ((actionLabel && actionLabel.length < 15) || !actionLabel) {
             return {
                 messageContainer: {
                     flexDirection: 'row',
-                    alignItems: 'center',
-                    justifyContent: 'space-between',
+                    justifyContent: 'center',
                 },
                 actionContainer: {
                     flexDirection: 'row',
+                    alignItems: 'center',
                 },
             };
         } else {
@@ -44,6 +46,8 @@ export const SnackBar = ({
                 actionContainer: {
                     flexDirection: 'row',
                     justifyContent: 'flex-end',
+
+                    width: '100%',
                     marginRight: theme.sw.spacing.xl,
                     marginTop: theme.sw.spacing.s,
                 },
@@ -61,7 +65,6 @@ export const SnackBar = ({
         snackBar: {
             backgroundColor: theme.sw.colors.neutral[800],
         },
-
         message: {
             flex: 1,
             color: theme.sw.colors.neutral[50],
@@ -78,15 +81,21 @@ export const SnackBar = ({
             <Snackbar
                 duration={duration}
                 visible={visible}
-                onDismiss={onDismiss}
+                onDismiss={onClose}
                 style={styles.snackBar}
             >
                 <View style={messageContainer as ViewStyle}>
                     <Text style={styles.message}>{message}</Text>
-                    <Pressable hitSlop={8} style={actionContainer as ViewStyle} onPress={onDismiss}>
-                        {actionLabel && <Text style={styles.actionLabel}>{actionLabel}</Text>}
-                        {iconName && <Icon name={iconName} color={theme.sw.colors.primary[50]} />}
-                    </Pressable>
+                    <View style={actionContainer as ViewStyle}>
+                        <Pressable hitSlop={8} onPress={onDismiss}>
+                            {actionLabel && <Text style={styles.actionLabel}>{actionLabel}</Text>}
+                        </Pressable>
+                        {iconName && (
+                            <Pressable hitSlop={8} onPress={onClose}>
+                                <Icon name={iconName} color={theme.sw.colors.primary[50]} />
+                            </Pressable>
+                        )}
+                    </View>
                 </View>
             </Snackbar>
         </View>
