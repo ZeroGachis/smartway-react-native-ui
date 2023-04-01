e6f7ea038539cbf23d716e564cacaf657dcf7956
Vadim Saranov <jolantacizyte@MBP-Jolanta.lan>
2023-04-01 23:16:57 +0300

    :recycle: Small refactor

--- example/src/ModifyQuantity/ModifyQuantityPage.tsx
+++ example/src/ModifyQuantity/ModifyQuantityPage.tsx
@@ -2,19 +2,9 @@ import React, { useState } from 'react';
 import { StyleSheet } from 'react-native';
 import { ModifyQuantity, Screen, useTheme } from 'smartway-react-native-ui';
 
-const product = {
-    label: 'Crème dessert chocolat avec  dessert chocolat avec avec.',
-    scanDate: '16:40',
-    initialPrice: '2.5€',
-    barcode: '349123451234',
-    quantity: 1,
-    discount: '-30%',
-    finalPrice: '1.75€',
-};
-
 export const ModifyQuantityPage = () => {
     const theme = useTheme();
-    const [value, setValue] = useState(product);
+    const [value, setValue] = useState<number>(0);
 
     const styles = StyleSheet.create({
         container: {
@@ -27,7 +17,7 @@ export const ModifyQuantityPage = () => {
         <Screen style={styles.container}>
             <ModifyQuantity
                 onValueChange={setValue}
-                value={value.quantity}
+                value={value}
                 minValue={0}
                 maxValue={6}
                 text={'Stick'}
