cb7073a70d485504544e68ab9e31634a378682f1
Vadim Saranov <jolantacizyte@MacBook-Pro-Jolanta.local>
2023-04-18 11:16:50 +0300

    :lipstick: Fix dialog screen after design review

--- example/src/Dialog/DialogPage.tsx
+++ example/src/Dialog/DialogPage.tsx
@@ -1,32 +1,37 @@
 import React, { useState } from 'react';
-import { StyleSheet } from 'react-native';
+import { StatusBar, StyleSheet } from 'react-native';
 import { Button, Dialog, Screen } from 'smartway-react-native-ui';
 
 export const DialogPage = () => {
-    const [modalVisible, setModalVisible] = useState<boolean>(false);
-
-    const showModal = () => {
-        setModalVisible(true);
-    };
-
-    const hideModal = () => {
-        setModalVisible(false);
-    };
+    const [singleOptionDialog, setSingleOptionDialog] = useState<boolean>(false);
+    const [twoOptionsDialog, setTwoOptionsDialog] = useState<boolean>(false);
 
     return (
         <Screen style={styles.container}>
-            <Button mode="text" onClick={showModal}>
-                Click me
+            <StatusBar backgroundColor={'transparent'} />
+            <Button mode="text" onClick={() => setSingleOptionDialog(true)}>
+                single option
+            </Button>
+            <Button mode="text" onClick={() => setTwoOptionsDialog(true)}>
+                two options
             </Button>
             <Dialog
-                visible={modalVisible}
+                visible={singleOptionDialog}
                 title={'Titre du dialog'}
-                content={'Contenu texte'}
+                content={'Contenu texte du dialog pour expliquer l’action à faire '}
+                confirmButtonLabel={'Valider'}
+                onConfirm={() => setSingleOptionDialog(false)}
+                onDismiss={() => setSingleOptionDialog(false)}
+            />
+            <Dialog
+                visible={twoOptionsDialog}
+                title={'Titre du dialog'}
+                content={'Contenu texte du dialog pour expliquer l’action à faire '}
                 dismissButtonLabel={'Annuler'}
-                confirmButtonLabel={'Ok'}
-                onDismiss={hideModal}
-                onConfirm={hideModal}
-            ></Dialog>
+                confirmButtonLabel={'Valider'}
+                onDismiss={() => setTwoOptionsDialog(false)}
+                onConfirm={() => setTwoOptionsDialog(false)}
+            />
         </Screen>
     );
 };
