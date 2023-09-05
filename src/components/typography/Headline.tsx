d1462f337331384344f77623f4cdddcc9608bd55
Nicolas Pieuchot <nls.pct@laposte.net>
2023-09-05 14:15:27 +0200

    0.8.9

--- src/components/typography/Headline.tsx
+++ src/components/typography/Headline.tsx
@@ -3,7 +3,7 @@ import { StyleProp, Text, TextStyle } from 'react-native';
 import { useTheme } from '../../styles/themes';
 
 export interface HeadlineProps {
-    size?: 'h1' | 'h2' | 'h3' | 'h4';
+    size?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5';
     children?: ReactNode;
     style?: TextStyle;
     testID?: string;
