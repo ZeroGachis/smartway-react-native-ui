7ac601719f8065caabeea82f2740ba293fa1f813
Adrien Castagliola <a.castagliola@smartway.ai>
2024-03-22 08:34:46 +0100

    📝 Color - display color hexa value on story

--- Storybook/components/Colors/Colors.stories.tsx
+++ Storybook/components/Colors/Colors.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react-native';
 import React from 'react';
-import { ScrollView, Text } from 'react-native';
+import { ScrollView, Text, View } from 'react-native';
 import { Badge } from 'react-native-paper';
 import { theme } from '../../../src/styles/themes';
 import { Headline } from 'smartway-react-native-ui';
@@ -12,6 +12,22 @@ export default {
 } as Meta<ComponentProps>;
 
 type Story = StoryObj<ComponentProps>;
+
+const ColorItem = ({ token, value, color }) => {
+    return (
+        <View
+            style={{
+                justifyContent: 'space-between',
+                flexDirection: 'row',
+                backgroundColor: value,
+                padding: 8,
+            }}
+        >
+            <Text style={{ color }}>{token}</Text>
+            <Text style={{ color }}>{value}</Text>
+        </View>
+    );
+};
 
 export const Default: Story = {
     render: () => {
@@ -28,110 +44,83 @@ export const Default: Story = {
                 <Headline size='h1'>Neutral</Headline>
                 {neutral.map(([key, hex]) => {
                     return (
-                        <Text
-                            style={{
-                                backgroundColor: hex,
-                                padding: 8,
-                                color: Number(key) <= 300 ? '#000' : undefined,
-                            }}
+                        <ColorItem
+                            color={Number(key) <= 300 ? '#000' : undefined}
+                            value={hex}
+                            token={key}
                             key={key}
-                        >
-                            {key}
-                        </Text>
+                        />
                     );
                 })}
                 <Headline size='h1'>Primary</Headline>
                 {primary.map(([key, hex]) => {
                     return (
-                        <Text
-                            style={{
-                                backgroundColor: hex,
-                                padding: 8,
-                                color: Number(key) <= 200 ? '#000' : undefined,
-                            }}
+                        <ColorItem
+                            color={Number(key) <= 200 ? '#000' : undefined}
+                            value={hex}
+                            token={key}
                             key={key}
-                        >
-                            {key}
-                        </Text>
+                        />
                     );
                 })}
                 <Headline size='h1'>Secondary</Headline>
                 {secondary.map(([key, hex]) => {
                     return (
-                        <Text
-                            style={{
-                                backgroundColor: hex,
-                                padding: 8,
-                                color: Number(key) <= 200 ? '#000' : undefined,
-                            }}
+                        <ColorItem
+                            color={Number(key) <= 200 ? '#000' : undefined}
+                            value={hex}
+                            token={key}
                             key={key}
-                        >
-                            {key}
-                        </Text>
+                        />
                     );
                 })}
                 <Headline size='h1'>Information</Headline>
                 {information.map(([key, hex]) => {
                     return (
-                        <Text
-                            style={{
-                                backgroundColor: hex,
-                                padding: 8,
-                                color: Number(key) <= 400 ? '#000' : undefined,
-                            }}
+                        <ColorItem
+                            color={Number(key) <= 400 ? '#000' : undefined}
+                            value={hex}
+                            token={key}
                             key={key}
-                        >
-                            {key}
-                        </Text>
+                        />
                     );
                 })}
                 <Headline size='h1'>Success</Headline>
                 {success.map(([key, hex]) => {
                     return (
-                        <Text
-                            style={{
-                                backgroundColor: hex,
-                                padding: 8,
-                                color: Number(key) <= 300 ? '#000' : undefined,
-                            }}
+                        <ColorItem
+                            color={Number(key) <= 300 ? '#000' : undefined}
+                            value={hex}
+                            token={key}
                             key={key}
-                        >
-                            {key}
-                        </Text>
+                        />
                     );
                 })}
                 <Headline size='h1'>Warning</Headline>
                 {warning.map(([key, hex]) => {
                     return (
-                        <Text
-                            style={{
-                                backgroundColor: hex,
-                                padding: 8,
-                                color: Number(key) <= 500 ? '#000' : undefined,
-                            }}
+                        <ColorItem
+                            color={Number(key) <= 500 ? '#000' : undefined}
+                            value={hex}
+                            token={key}
                             key={key}
-                        >
-                            {key}
-                        </Text>
+                        />
                     );
                 })}
                 <Headline size='h1'>Error</Headline>
                 {error.map(([key, hex]) => {
                     return (
-                        <Text
-                            style={{
-                                backgroundColor: hex,
-                                padding: 8,
-                                color: Number(key) <= 500 ? '#000' : undefined,
-                            }}
+                        <ColorItem
+                            color={Number(key) <= 500 ? '#000' : undefined}
+                            value={hex}
+                            token={key}
                             key={key}
-                        >
-                            {key}
-                        </Text>
+                        />
                     );
                 })}
             </ScrollView>
         );
     },
 };
+
 Default.parameters = { noSafeArea: false };
