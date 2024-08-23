5f68adf4e2cceb3c336e13d578c4038c2703645e
Valentin Magrez <valentin.magrez@gmail.com>
2024-08-23 11:36:03 +0200

    :lipstick: Add line height property to body
    Because utils method to convert lineheight token to lineheight style is here

--- src/components/typography/Body.tsx
+++ src/components/typography/Body.tsx
@@ -3,21 +3,33 @@ import { StyleProp, Text, TextStyle } from 'react-native';
 import type { TextProps } from 'react-native-paper';
 import { useTheme } from '../../styles/themes';
 import { tokens } from '@zerogachis/smartway-design-token';
-import { getFont } from './utils';
+import { LineHeight } from '@zerogachis/smartway-design-token/dist/cjs/src/Tokens/TokensType';
+import { getFont, getLineHeight } from './utils';
 
 type BodyTypography = keyof typeof tokens.typography.body;
 
 export interface BodyProps extends TextProps<Text> {
     typography?: BodyTypography;
+    lineHeight?: keyof LineHeight;
 }
 
-export const Body = ({ typography = 'n4', children, style, ...props }: BodyProps) => {
+export const Body = ({
+    typography = 'n4',
+    lineHeight = 'normal',
+    children,
+    style,
+    ...props
+}: BodyProps) => {
     const theme = useTheme();
 
     const bodyStyle: StyleProp<TextStyle> = {
         color: theme.sw.color.neutral[800],
         fontSize: tokens.typography.body[typography]?.fontSize,
         fontFamily: getFont(tokens.typography.body[typography]),
+        lineHeight: getLineHeight(
+            tokens.lineHeight[lineHeight],
+            tokens.typography.headline[typography]?.fontSize,
+        ),
     };
 
     return (
